300262d0ad760ca4799b27781e52c324172c93d3
Andreas K. Brandhøj <akbr18@student.aau.dk>
2021-03-20 18:21:15 +0100

    Removed QuadDynArr

--- src/value.h
+++ src/value.h
@@ -9,21 +9,4 @@ typedef int16_t vm_word_t;
 typedef int32_t vm_double_t;
 typedef int64_t vm_quad_t;
 
-/* The constant pool for values.
- * The instruction to load a constant
- * looks up the value by index in that array. */
-typedef struct {
-    uint32_t capacity;
-    uint32_t count;
-    vm_quad_t* values;
-} QuadWordArray;
-
-
-
-void initQuadWordArray(QuadWordArray* array);
-void writeValueArray(QuadWordArray* array, vm_quad_t value);
-void freeValueArray(QuadWordArray* array);
-
-void printValue(vm_quad_t value);
-
 #endif
